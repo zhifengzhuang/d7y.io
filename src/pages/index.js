49eacaa3e63877844b6296eeb556f02f9255d0f1
Gaius <gaius.qi@gmail.com>
2022-02-22 11:28:53 +0800

    feat: add whatis
    Signed-off-by: Gaius <gaius.qi@gmail.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -38,7 +38,17 @@ export default function Home() {
 
       <BasicInfo />
 
-      <main className={clsx("hero", styles.hero)}></main>
+      <main className={clsx("hero", styles.hero)}>
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <h1 className={styles.featureTitle}>
+                <Translate>Why Dragonfly?</Translate>
+              </h1>
+            </div>
+          </div>
+        </div>
+      </main>
 
       <div className={clsx("hero", styles.hero)}>
         <div className="container text--center">
@@ -72,71 +82,75 @@ const BasicInfo = () => (
     <div className="container">
       <div className="row">
         <div className="col col--6">
-          <h1 className={styles.basicTitle}>
-            <img
-              className={styles.questionIcon}
-              src={useBaseUrl("img/icon/question.svg")}
-              alt="question"
-            />
-            <Translate>What is Dragonfly?</Translate>
-          </h1>
-          <p className={clsx("hero__subtitle", styles.description)}>
-            <Translate>
-              Dragonfly is an open source intelligent P2P based image and file
-              distribution system. Its goal is to tackle all distribution
-              problems in cloud native scenarios. It is now hosted by the Cloud
-              Native Computing Foundation (CNCF) as an Incubating Level Project.
-              Originally it was born to solve all kinds of distribution at very
-              large scales, such as application distribution, cache
-              distribution, log distribution, image distribution, and so on.
-            </Translate>
-          </p>
+          <div className="row">
+            <h1 className={styles.basicTitle}>
+              <img
+                className={styles.questionIcon}
+                src={useBaseUrl("img/icon/question.svg")}
+                alt="question"
+              />
+              <Translate>What is Dragonfly?</Translate>
+            </h1>
+          </div>
+
+          <div className="row">
+            <p className={clsx("hero__subtitle", styles.description)}>
+              <Translate>
+                Dragonfly is an open source intelligent P2P based image and file
+                distribution system. Its goal is to tackle all distribution
+                problems in cloud native scenarios. It is now hosted by the
+                Cloud Native Computing Foundation (CNCF) as an Incubating Level
+                Project. Originally it was born to solve all kinds of
+                distribution at very large scales, such as application
+                distribution, cache distribution, log distribution, image
+                distribution, and so on.
+              </Translate>
+            </p>
+          </div>
+
+          <div className={clsx("row", styles.badgeWrapper)}>
+            <GitHubButton
+              href="https://github.com/dragonflyoss/Dragonfly2"
+              data-icon="octicon-star"
+              data-show-count="true"
+              aria-label="Star dragonflyoss/Dragonfly2 on GitHub"
+            >
+              Star
+            </GitHubButton>
+            <GitHubButton
+              href="https://github.com/dragonflyoss/Dragonfly2/subscription"
+              data-icon="octicon-eye"
+              data-show-count="true"
+              aria-label="Watch dragonflyoss/Dragonfly2 on GitHub"
+            >
+              Watch
+            </GitHubButton>
+            <GitHubButton
+              href="https://github.com/dragonflyoss/Dragonfly2/fork"
+              data-icon="octicon-repo-forked"
+              data-show-count="true"
+              aria-label="Fork dragonflyoss/Dragonfly2 on GitHub"
+            >
+              Fork
+            </GitHubButton>
+          </div>
+          <div className={clsx("row", styles.badgeWrapper)}>
+            <a href="https://github.com/dragonflyoss/Dragonfly2/releases/">
+              <img src="https://img.shields.io/github/release/dragonflyoss/Dragonfly2.svg" />
+            </a>
+            <a href="https://www.cncf.io/projects/">
+              <img src="https://img.shields.io/badge/CNCF%20Status-Incubating-informational" />
+            </a>
+          </div>
         </div>
-        <div className="col">
+
+        <div className={clsx("col", styles.architectureImage)}>
           <img
-            className="image"
-            src={useBaseUrl("img/what-is-kubevela.png")}
+            className={styles.architectureImage}
+            src={useBaseUrl("img/landing/architecture.png")}
             align="right"
             alt="what is dragonfly"
           />
-        </div>
-      </div>
-      <div className={clsx("row")}>
-        <div className={clsx("col col--6", styles.badgeWrapper)}>
-          <GitHubButton
-            href="https://github.com/dragonflyoss/Dragonfly2"
-            data-icon="octicon-star"
-            data-show-count="true"
-            aria-label="Star dragonflyoss/Dragonfly2 on GitHub"
-          >
-            Star
-          </GitHubButton>
-          <GitHubButton
-            href="https://github.com/dragonflyoss/Dragonfly2/subscription"
-            data-icon="octicon-eye"
-            data-show-count="true"
-            aria-label="Watch dragonflyoss/Dragonfly2 on GitHub"
-          >
-            Watch
-          </GitHubButton>
-          <GitHubButton
-            href="https://github.com/dragonflyoss/Dragonfly2/fork"
-            data-icon="octicon-repo-forked"
-            data-show-count="true"
-            aria-label="Fork dragonflyoss/Dragonfly2 on GitHub"
-          >
-            Fork
-          </GitHubButton>
-        </div>
-      </div>
-      <div className="row">
-        <div className={clsx("col col--6", styles.badgeWrapper)}>
-          <a href="https://github.com/dragonflyoss/Dragonfly2/releases/">
-            <img src="https://img.shields.io/github/release/dragonflyoss/Dragonfly2.svg" />
-          </a>
-          <a href="https://www.cncf.io/projects/">
-            <img src="https://img.shields.io/badge/CNCF%20Status-Incubating-informational" />
-          </a>
         </div>
       </div>
     </div>
